8a4c22b82cbfda3d54cc22fd2818f1701f828ab1
Scott-Eisenberg <105310924+Scott-Eisenberg@users.noreply.github.com>
2026-04-01 20:04:13 -0700

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,27 @@
+#nums -> frequency (unsorted) -> sorted array. Return reverse
+#[1,2,2,3,3,3] -> [1:1, 2:2, 3:3] -> [1,2,3]
+class Solution:
+    def topKFrequent(self, nums:List[int], k:int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+        ret = []
+        for i in range(len(freq)-1, 0, -1):
+            for num in freq[i]:
+                ret.append(num)
+                if len(ret) == k:
+                    return ret
+
+    
+
+
+
+
+
+            
+
+        
+        
